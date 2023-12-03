6d49e2a1cc4159737a2a4934b928017a10d06a70
Ahmad Ruhaifi <ahmadruhaifi@gmail.com>
2023-12-03 23:09:25 +0800

    Added modal for movie details.

---
+++ components/Modal.tsx
@@ -0,0 +1,103 @@
+import React, { useEffect, useState } from 'react';
+import Image from 'next/image';
+
+interface ModalProps {
+  isOpen: boolean;
+  onClose: () => void;
+  movie: Movie;
+}
+
+interface Movie {
+  id: string;
+  title: string;
+  overview: string;
+  release_date: string;
+}
+
+interface Actor {
+  profile_path: string | null;
+  name: string;
+  character: string;
+}
+
+const Modal: React.FC<ModalProps> = ({ isOpen, onClose, movie }) => {
+  const [cast, setCast] = useState<Actor[]>([]);
+
+  useEffect(() => {
+    const fetchCast = async () => {
+      if (movie?.id) {
+        try {
+          const response = await fetch(
+            `https://api.themoviedb.org/3/movie/${movie.id}/credits?api_key=${process.env.NEXT_PUBLIC_API_KEY}`
+          );
+          const data = await response.json();
+          setCast(data.cast);
+        } catch (error) {
+          console.error("Error fetching cast:", error);
+        }
+      }
+    };
+
+    if (isOpen) {
+      fetchCast();
+    }
+  }, [isOpen, movie]);
+
+  if (!isOpen || !movie) return null;
+
+  return (
+    <div className="fixed inset-0 bg-gray-600 bg-opacity-50 overflow-y-auto h-full w-full">
+      <div className="relative top-20 mx-auto p-5 border w-1/2 shadow-lg rounded-md bg-white">
+        <h3 className="text-lg leading-6 font-medium text-gray-900 underline">{movie.title}</h3>
+        {/* Movie details */}
+        <div className="mt-3">
+          <p className="text-sm text-gray-500 mb-2">
+            <span className="font-bold">Synopsis:</span> {movie.overview}
+          </p>
+          <p className="text-sm text-gray-500 mb-2">
+            <span className="font-bold">Release date:</span> {movie.release_date}
+          </p>
+        </div>
+        {/* Cast details */}
+        <div className="mt-2">
+          <p className="text-sm text-gray-500 mb-2 font-bold">Cast:</p>
+          <div className="overflow-auto max-h-64">
+            <ul className="list-disc list-inside text-sm text-gray-500">
+              {cast.map((actor, index) => (
+                <li key={index} className="flex items-center mb-2">
+                  {actor.profile_path ? (
+                    <Image
+                      src={`https://image.tmdb.org/t/p/w500${actor.profile_path}`}
+                      alt={actor.name}
+                      width={50}
+                      height={50}
+                      className="rounded-full mr-2"
+                    />
+                  ) : (
+                    <div className="w-12 h-12 rounded-full bg-gray-200 mr-2 flex items-center justify-center">
+                      {/* Default placeholder actor's initials */}
+                      <span>{actor.name.charAt(0)}</span>
+                    </div>
+                  )}
+                  <span>{actor.name} as {actor.character}</span>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+        {/* Close button */}
+        <div className="flex justify-end py-3">
+          <button
+            id="ok-btn"
+            className="px-4 py-2 bg-blue-500 text-white text-base font-medium rounded-md w-5/12 shadow-sm hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-300"
+            onClick={onClose}
+          >
+            Close
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Modal;
--- components/ResultItems.tsx
+++ components/ResultItems.tsx
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Card from "./Card";
 import Image from "next/image";
 import { FaRegThumbsUp } from "react-icons/fa";
 import { AiFillStar } from "react-icons/ai";
+import Modal from './Modal';
 
 interface Result {
   id: string;
@@ -21,36 +22,59 @@ interface ResultItemsProps {
 }
 
 const ResultItems: React.FC<ResultItemsProps> = ({ results }) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+
   return (
     <Card>
-      <Image
-        className="rounded-t-lg group-hover:opacity-70 transition-opacity duration-200"
-        src={`https://image.tmdb.org/t/p/w500${
-          results?.backdrop_path || results?.poster_path
-        }`}
-        width={500}
-        height={300}
-        style={{ maxWidth: "100%", maxHeight: "160px" }}
-        placeholder="blur"
-        blurDataURL="/spinner.svg"
-        alt="Image is not available"
-      />
-      <div className="px-2">
-        <h2 className="truncate text-lg font-bold text-cyan-500 my-2">
-          {results?.title || results?.original_title}
-        </h2>
-        <div className="flex justify-between items-center flex-wrap">
-          <p className="mt-1">{results?.release_date}</p>
-          <p className="flex items-center mt-1 mx-1">
-            <FaRegThumbsUp className="mr-1 text-cyan-500" />
-            {results?.vote_count}
-          </p>
-          <p className="flex items-center mt-1">
-            <AiFillStar className="text-amber-500 mr-1" />
-            {Number(results?.vote_average).toFixed(1)}
-          </p>
+      <div onClick={openModal}>
+        <Image
+          className="rounded-t-lg group-hover:opacity-70 transition-opacity duration-200"
+          src={`https://image.tmdb.org/t/p/w500${
+            results?.backdrop_path || results?.poster_path
+          }`}
+          width={500}
+          height={300}
+          style={{ maxWidth: "100%", maxHeight: "160px" }}
+          placeholder="blur"
+          blurDataURL="/spinner.svg"
+          alt="Picture not found"
+        />
+        <div className="px-2">
+          <h2 className="truncate text-lg font-bold text-cyan-500 my-2">
+            {results?.title || results?.original_title}
+          </h2>
+          <div className="flex justify-between items-center flex-wrap">
+            <p className="mt-1">{results?.release_date}</p>
+            <p className="flex items-center mt-1 mx-1">
+              <FaRegThumbsUp className="mr-1 text-cyan-500" />
+              {results?.vote_count}
+            </p>
+            <p className="flex items-center mt-1">
+              <AiFillStar className="text-amber-500 mr-1" />
+              {Number(results?.vote_average).toFixed(1)}
+            </p>
+          </div>
         </div>
       </div>
+
+      <Modal 
+        isOpen={isModalOpen} 
+        onClose={closeModal} 
+        movie={{
+          id: results?.id,
+          title: results?.title || 'Unknown Title',
+          overview: results?.overview || 'Unknown Overview',
+          release_date: results?.release_date || 'Unknown Date',
+        }}
+      />
     </Card>
   );
 };
