5dff60d593c4bafeb6384615795a327e1dd81d76
Ahmad Ruhaifi <ahmadruhaifi@gmail.com>
2023-12-04 15:02:17 +0800

    Added function to filter by genre & rating.

--- app/page.tsx
+++ app/page.tsx
@@ -3,32 +3,72 @@
 import React, { useEffect, useState } from "react";
 import Results from "../components/Results";
 import Pagination from "../components/Pagination";
+import Filter from "../components/Filter";
 import { MovieData, HomeProps } from "../utils/interfaces"
 
 const Home: React.FC<HomeProps> = ({ searchParams }) => {
   const [data, setData] = useState<MovieData | null>(null);
   const pageNumber = searchParams.page || 1;
+  const [genres, setGenres] = useState([]);
+  const [selectedGenre, setSelectedGenre] = useState('');
+  const [selectedRating, setSelectedRating] = useState(0);
 
+  // Fetch the genres
+  useEffect(() => {
+    const fetchGenres = async () => {
+      try {
+        const res = await fetch(`https://api.themoviedb.org/3/genre/movie/list?api_key=${process.env.NEXT_PUBLIC_API_KEY}`);
+
+        if (!res.ok) throw new Error("Unable to retrieve genres.");
+
+        const result = await res.json();
+        setGenres(result.genres);
+      } catch (error) {
+        console.error("Encountered an issue while retrieving genres: ", error);
+      }
+    };
+  
+    fetchGenres();
+  }, []);
+
+  // Fetch the movies
   useEffect(() => {
     const fetchData = async () => {
       try {
-        // Fetch the records
-        const res = await fetch(`https://api.themoviedb.org/3/movie/now_playing?api_key=${process.env.NEXT_PUBLIC_API_KEY}&language=en-Us&page=${pageNumber}`);
+        let apiEndpoint = `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.NEXT_PUBLIC_API_KEY}&page=${pageNumber}`;
 
-        if (!res.ok) throw new Error("Unable to retrieve data.");
+        if (selectedGenre) {
+          apiEndpoint += `&with_genres=${selectedGenre}`;
+        }
+        if (selectedRating > 0) {
+          apiEndpoint += `&vote_average.gte=${selectedRating}`;
+        }
+
+        const res = await fetch(apiEndpoint);
+
+        if (!res.ok) throw new Error("Unable to retrieve movies.");
 
         const result: MovieData = await res.json();
         setData(result);
       } catch (error) {
-        console.error("Encountered an issue while retrieving data: ", error);
+        console.error("Encountered an issue while retrieving movies: ", error);
       }
     };
 
     fetchData();
-  }, [pageNumber]);
+  }, [pageNumber, selectedGenre, selectedRating]);
 
   return (
     <>
+      {/* Genre Selector */}
+      <Filter 
+        genres={genres}
+        selectedGenre={selectedGenre}
+        setSelectedGenre={setSelectedGenre}
+        selectedRating={selectedRating}
+        setSelectedRating={setSelectedRating}
+      />
+      {/* Results and Pagination */}
       {data && <Results results={data.results} />}
       {data?.total_results && data?.total_pages && (
         <Pagination totalPages={data?.total_pages} />
